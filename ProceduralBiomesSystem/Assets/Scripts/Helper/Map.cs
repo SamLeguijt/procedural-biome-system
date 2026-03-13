29f2ec4ee34f9bebc17c534aebeaf39561564d8b
samleguijt <sam.leguijt@gmail.com>
2026-03-13 16:30:31 +0100

    Added a generic map class for use for different maps

---
+++ ProceduralBiomesSystem/Assets/Scripts/Helper/Map.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map<T>
+{
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+    public T[,] Values { get; private set; }
+
+
+    public Map(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        Values = new T[width, height];
+    }
+
+    public T this[int x, int y]
+    {
+        get => Values[x, y];
+        set => Values[x, y] = value;
+    }
+
+    public bool Contains(int x, int y)
+    {
+        if (!IsInBounds(x, y))
+            return false;
+
+        return Values[x, y] != null;
+    }
+
+    bool IsInBounds(int x, int y)
+    {
+        return (x >= 0 && x < Width) && (y >= 0 && y < Height);
+    }
+}
+
